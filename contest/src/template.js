4937a5d5737d3aed26fd62b7f328964d4a41899c
Ronak Malik <ronakmalik4242@gmail.com>
2019-09-29 14:25:51 -0500

    Fix template so it better reflects a template page

--- contest/src/template.js
+++ contest/src/template.js
@@ -1,22 +1,25 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { MDBContainer } from 'mdbreact';
 
 
 
-
-class template extends Component
-{
+class template extends Component {
     constructor(props) {
         super(props);
         this.state = {
-          
+
         };
     }
-    render(){
-        return(
+    render() {
+        return (
             <div>
-                
+                <MDBContainer className="text-center mt-5">
+                    <h2>Template</h2>
+                    <h5>Test Page</h5>
+                </MDBContainer>
             </div>
         );
     }
-    
+
 }
+export default template;
