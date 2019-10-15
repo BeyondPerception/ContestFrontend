46fdab7c75880bddfea6ee7fc86546fe1be7498a
ral <rahul.shanmugha@gmail.com>
2019-10-15 15:50:06 -0500

    started profile page

--- contest/src/Profile.js
+++ contest/src/Profile.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { MDBContainer } from 'mdbreact';
+import { MDBContainer, MDBRow, MDBCol } from 'mdbreact';
 
 
 
@@ -15,7 +15,21 @@ class Profile extends Component {
             <div>
                 <MDBContainer className="text-center mt-5">
                     <h2>Profile</h2>
-                    <h5>This page will be available on the day of the contest.</h5>
+                    <MDBRow>
+                        <MDBCol >
+                        <h1>Hi</h1>
+                        <br/>
+                        <h2>School Name: </h2>
+                        <br/>
+                        <h3>Hi</h3>
+                        <br/>
+                        <h4>Hi</h4>
+                        <br/>
+                        <h5>Hi</h5>
+                        <br/>
+                        <h6>Hi</h6>
+                        </MDBCol>
+                    </MDBRow>
                 </MDBContainer>
             </div>
         );
