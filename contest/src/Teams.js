9a830e4087590f01731f4b47d62f5a1f8b112d2f
ral <rahul.shanmugha@gmail.com>
2019-10-20 21:26:51 -0500

    Finished teams.js

--- contest/src/Teams.js
+++ contest/src/Teams.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { MDBContainer, MDBRow, MDBCol, MDBInputGroup } from 'mdbreact';
+import { MDBContainer, MDBRow, MDBCol, MDBInputGroup, MDBBtn } from 'mdbreact';
 
 
 
@@ -12,35 +12,90 @@ class Teams extends Component {
     }
     render() {
         return (
-            <div>
                 <MDBContainer className="text-center mt-5">
                     <h1>Teams</h1>
                     <MDBRow>
                         <h2><b>Edit Students</b></h2>
                     </MDBRow>
                     <br/>
-                    
-                    <MDBCol style={{width:'20%'}}>
-                        <label>Select Division</label>
-                        <select className="browser-default custom-select">
-                            <option value="0">Choose...</option>
-                            <option value="N">Novice</option>
-                            <option value="A">Advanced</option>
-                        </select>
-                    </MDBCol >
-                    <br/><br/>
-                    
-                    <MDBCol>
-                    <label for="username">First Name   </label>
-                    <input id="user_first_name" name="user[first_name]" size="30" type="text" />
+                    <MDBRow>
+                        <MDBCol>
+                            <label>Select Division</label>
+                            <select className="browser-default custom-select">
+                                <option value="0">Choose...</option>
+                                <option value="N">Novice</option>
+                                <option value="A">Advanced</option>
+                            </select>
+                        </MDBCol >
 
-                    <label for="name">Last Name</label>
-                    <input id="user_last_name" name="user[last_name]" size="30" type="text" />
-                    </MDBCol>
+                        <MDBCol>
+                        <label>Enter First Name</label>
+                        <MDBInputGroup></MDBInputGroup>
+                        </MDBCol>
+                    
+                        <MDBCol>
+                        <label>Enter Last Name</label>
+                        <MDBInputGroup></MDBInputGroup>
+                        </MDBCol>
+
+                       <MDBBtn outline color='danger'> Remove Student</MDBBtn>
+
+                    </MDBRow>
+                    <br/>
+                    <MDBRow>
+                        <MDBBtn color='red'>Add Student</MDBBtn>
+                    </MDBRow>
+                    <br/><br/>
+                    <MDBRow>
+                    <h2><b>Edit Teams</b></h2>
+                    </MDBRow>
+                    <br/>
+                    <MDBRow>
+                        <MDBCol style={{width:"10%"}}>
+                            <label>Select Division</label>
+                            <select className="browser-default custom-select">
+                                <option value="0">Choose...</option>
+                                <option value="N">Novice</option>
+                                <option value="A">Advanced</option>
+                            </select>
+                        </MDBCol >
+
+                        <MDBCol>
+                        <label>Select Team Members</label>
+                            <select className="browser-default custom-select">
+                                
+                            </select>
+                            <br/>
+                            <select className="browser-default custom-select">
+                                
+                            </select>
+                            <select className="browser-default custom-select">
+                                
+                            </select>
+
+                        </MDBCol>
+
+                        <MDBBtn outline color='danger'>X</MDBBtn>
+
+                    </MDBRow>
+
+                    <br/>
+                    <MDBRow>
+                        <MDBBtn color='red'>Add Team</MDBBtn>
+                    </MDBRow>
+                    <br/><br/>
+                    <MDBRow>
+                        <h4><b>Total Cost: </b></h4>
+                    </MDBRow>
+                    <br/><br/>
+                    <MDBRow>
+                        <MDBBtn color='red'>Save</MDBBtn>
+                    </MDBRow>
+
+
                         
                    
                 </MDBContainer>
-            </div>
         );
     }
 
