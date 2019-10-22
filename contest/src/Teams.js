4a81a2f5cd85215029305a2c81ae12cd7a8d40b2
Ronak Malik <ronakmalik4242@gmail.com>
2019-10-21 19:58:44 -0500

    did teams stuff

--- contest/src/Teams.js
+++ contest/src/Teams.js
@@ -12,90 +12,87 @@ class Teams extends Component {
     }
     render() {
         return (
-                <MDBContainer className="text-center mt-5">
-                    <h1>Teams</h1>
-                    <MDBRow>
-                        <h2><b>Edit Students</b></h2>
-                    </MDBRow>
-                    <br/>
-                    <MDBRow>
-                        <MDBCol>
-                            <label>Select Division</label>
-                            <select className="browser-default custom-select">
-                                <option value="0">Choose...</option>
-                                <option value="N">Novice</option>
-                                <option value="A">Advanced</option>
-                            </select>
-                        </MDBCol >
+            <MDBContainer className="text-center mt-5">
+                <h1>Teams</h1>
+                <MDBRow>
+                    <h2><b>Edit Students</b></h2>
+                </MDBRow>
+                <br />
+                <MDBRow>
+                    <MDBCol>
+                        <label>Select Division</label>
+                        <select className="browser-default custom-select">
+                            <option value="0">Choose...</option>
+                            <option value="N">Novice</option>
+                            <option value="A">Advanced</option>
+                        </select>
+                    </MDBCol >
 
-                        <MDBCol>
+                    <MDBCol>
                         <label>Enter First Name</label>
                         <MDBInputGroup></MDBInputGroup>
-                        </MDBCol>
-                    
-                        <MDBCol>
+                    </MDBCol>
+
+                    <MDBCol>
                         <label>Enter Last Name</label>
                         <MDBInputGroup></MDBInputGroup>
-                        </MDBCol>
+                    </MDBCol>
 
-                       <MDBBtn outline color='danger'> Remove Student</MDBBtn>
+                    <MDBBtn outline color='danger'> Remove Student</MDBBtn>
 
-                    </MDBRow>
-                    <br/>
-                    <MDBRow>
-                        <MDBBtn color='red'>Add Student</MDBBtn>
-                    </MDBRow>
-                    <br/><br/>
-                    <MDBRow>
+                </MDBRow>
+                <br />
+                <MDBRow>
+                    <MDBBtn color='red'>Add Student</MDBBtn>
+                </MDBRow>
+                <br /><br />
+                <MDBRow>
                     <h2><b>Edit Teams</b></h2>
-                    </MDBRow>
-                    <br/>
-                    <MDBRow>
-                        <MDBCol style={{width:"10%"}}>
-                            <label>Select Division</label>
-                            <select className="browser-default custom-select">
-                                <option value="0">Choose...</option>
-                                <option value="N">Novice</option>
-                                <option value="A">Advanced</option>
-                            </select>
-                        </MDBCol >
+                </MDBRow>
+                <br />
+                <MDBRow>
+                    <MDBCol style={{ width: "10%" }}>
+                        <label>Select Division</label>
+                        <select className="browser-default custom-select">
+                            <option value="0">Choose...</option>
+                            <option value="N">Novice</option>
+                            <option value="A">Advanced</option>
+                        </select>
+                    </MDBCol >
 
-                        <MDBCol>
+                    <MDBCol>
                         <label>Select Team Members</label>
-                            <select className="browser-default custom-select">
-                                
-                            </select>
-                            <br/>
-                            <select className="browser-default custom-select">
-                                
-                            </select>
-                            <select className="browser-default custom-select">
-                                
-                            </select>
+                        <select className="browser-default custom-select">
 
-                        </MDBCol>
+                        </select>
+                        <br />
+                        <select className="browser-default custom-select">
 
-                        <MDBBtn outline color='danger'>X</MDBBtn>
+                        </select>
+                        <select className="browser-default custom-select">
 
-                    </MDBRow>
+                        </select>
 
-                    <br/>
-                    <MDBRow>
-                        <MDBBtn color='red'>Add Team</MDBBtn>
-                    </MDBRow>
-                    <br/><br/>
-                    <MDBRow>
-                        <h4><b>Total Cost: </b></h4>
-                    </MDBRow>
-                    <br/><br/>
-                    <MDBRow>
-                        <MDBBtn color='red'>Save</MDBBtn>
-                    </MDBRow>
+                    </MDBCol>
 
+                    <MDBBtn outline color='danger'>X</MDBBtn>
 
-                        
-                   
-                </MDBContainer>
+                </MDBRow>
+
+                <br />
+                <MDBRow>
+                    <MDBBtn color='red'>Add Team</MDBBtn>
+                </MDBRow>
+                <br /><br />
+                <MDBRow>
+                    <h4><b>Total Cost: </b></h4>
+                </MDBRow>
+                <br /><br />
+                <MDBRow>
+                    <MDBBtn color='red'>Save</MDBBtn>
+                </MDBRow>
+
+            </MDBContainer>
         );
     }
 
