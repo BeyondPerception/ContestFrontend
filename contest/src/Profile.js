4529ca809abaab558be21d33d23db29a85206f24
Ronak Malik <ronakmalik4242@gmail.com>
2019-10-21 19:19:29 -0500

    Did stuff with Profile, dont really know what

--- contest/src/Profile.js
+++ contest/src/Profile.js
@@ -1,37 +1,45 @@
 import React, { Component } from 'react';
-import { MDBContainer, MDBRow, MDBCol, MDBCard, MDBCardHeader, MDBCardBody, MDBTable, MDBBtn} from 'mdbreact';
-
-
+import { MDBContainer, MDBRow, MDBCol, MDBCard, MDBCardHeader, MDBCardBody, MDBTable, MDBBtn } from 'mdbreact';
 
 class Profile extends Component {
-    
+
     constructor(props) {
         super(props);
         this.state = {
-
+            school: "",
+            name: "",
+            email: "",
         };
+        this.storeData = this.storeData.bind(this);
     }
+
+    storeData(data, key) {
+        this.setState({
+            [key]: data
+        });
+    }
+
     render() {
+        const { school, name, email } = this.props
         return (
             <div>
                 <MDBContainer>
-                    <br/><br/><br/>
+                    <br /><br /><br />
                     <h1>Profile</h1>
-                    <br/><br/>
+                    <br /><br />
                     <MDBRow >
                         <MDBCol >
-                            <h4>School Name: </h4>
-                            <h4>Coaches Name: </h4>
-                            <h4>Phone Number: </h4>
-                            <h4>Email: </h4>
-                            <br/>
+                            <h4>School Name: {school}</h4>
+                            <h4>Coaches Name: {name}</h4>
+                            <h4>Email: {email}</h4>
+                            <br />
                             <MDBBtn color='red' href='/Editinfo'>Edit Information</MDBBtn>
                         </MDBCol>
                     </MDBRow>
                     <MDBRow>
-                        
+
                     </MDBRow>
-                    
+
                 </MDBContainer>
             </div>
         );
